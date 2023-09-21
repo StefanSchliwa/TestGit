f36b5d2ffa5d93d1beb8bbb8d40761ad75e95401
Stefan Schliwa <stefan_alexander.schliwa@hsbi.de>
2023-09-21 17:42:20 +0200

    new Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Test Test Test 1 2 3!");
+        System.out.println("A new Test sout!");
     }
 }
